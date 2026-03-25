cb83447269de71fd542508fc32aef89e818749ec
potechimaru <87409411+potechimaru@users.noreply.github.com>
2026-03-26 08:23:16 +0900

    remove : Ruleコンポーネント群を削除

--- Assets/Scripts/8_Rule/Internal/IGameFinishRule.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UniRx;
-
-public interface IGameFinishRule : IDisposable
-{
-    GameFinishRuleType RuleType { get; }
-    IObservable<GameFinishSignal> OnTriggered { get; }
-    void Trigger(in GameFinishSignal signal);
-}
--- Assets/Scripts/8_Rule/Internal/IScoreRule.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UniRx;
-
-public interface IScoreRule : IDisposable
-{
-    ScoreRuleType RuleType { get; }
-    IObservable<ScoreRuleSignal> OnTriggered { get; }
-    void Evaluate(in ScoreEventContext ctx);
-}
